16989036705424bbc0c55239e168a90498f8bed3
Nasrul Fuad <nasrulfuad08@gmail.com>
2023-03-24 22:09:29 +0700

    test(user): :white_check_mark: add e2e test find user by id

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -5,11 +5,15 @@ import { AppModule } from '@src/app.module';
 import { UserUseCase } from '@src/modules/user/constants/user-usecase';
 import { UserRepository } from '@src/modules/user/repository/user.repository';
 import { CreateUserDto } from '@src/modules/user/web/dto/create-user.dto';
+import { UserEntity } from '@src/modules/user/web/entities/user.entity';
+import { UserEntityImpl } from '@src/modules/user/web/entities/user.entity-impl';
 import * as request from 'supertest';
 import { DataSource, EntityManager } from 'typeorm';
 
 describe('UserController', () => {
-  let app: INestApplication;
+  let app: INestApplication,
+    dataSource: DataSource,
+    userRepository: UserRepository<EntityManager>;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -19,82 +23,145 @@ describe('UserController', () => {
     app = moduleRef.createNestApplication();
     app = appConfiguration(app);
     await app.init();
+
+    const rd = getRepoAndDataSource(app);
+    dataSource = rd.dataSource;
+    userRepository = rd.userRepository;
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it('[GET] /api/v1/users ✅ Find All users', async () => {
-    const r: request.Response = await request(app.getHttpServer()).get('/api/v1/users');
+  describe('Find all users', () => {
+    it('[GET] /api/v1/users ✅ Find All users', async () => {
+      const r: request.Response = await request(app.getHttpServer()).get(
+        '/api/v1/users',
+      );
 
-    expect(r.statusCode).toBe(HttpStatus.OK);
-    expect(r.body).toEqual({
-      responseTime: expect.any(String),
-      statusCode: HttpStatus.OK,
-      message: 'Get all users successfully',
-      meta: {
-        page: 1,
-        perPage: 10,
-        totalItems: 0,
-        totalPage: 0,
-      },
-      data: [],
+      expect(r.statusCode).toBe(HttpStatus.OK);
+      expect(r.body).toEqual({
+        responseTime: expect.any(String),
+        statusCode: HttpStatus.OK,
+        message: 'Get all users successfully',
+        meta: {
+          page: 1,
+          perPage: 10,
+          totalItems: 0,
+          totalPage: 0,
+        },
+        data: [],
+      });
     });
   });
 
-  it('[POST] /api/v1/users ✅ Success create a user', async () => {
-    const createUserDto: CreateUserDto = {
-      name: 'test',
-      email: 'test@test.com',
-      age: 13,
-      password: 'test123',
-    };
+  describe('Create user', () => {
+    it('[POST] /api/v1/users ✅ Success create a user', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'test',
+        email: 'test@test.com',
+        age: 13,
+        password: 'test123',
+      };
 
-    const r: request.Response = await request(app.getHttpServer())
-      .post('/api/v1/users')
-      .send(createUserDto);
+      const r: request.Response = await request(app.getHttpServer())
+        .post('/api/v1/users')
+        .send(createUserDto);
 
-    expect(r.statusCode).toBe(HttpStatus.CREATED);
-    expect(r.body).toEqual({
-      responseTime: expect.any(String),
-      statusCode: HttpStatus.CREATED,
-      message: 'Create a user successfully',
-      data: {
-        id: expect.any(String),
-        email: createUserDto.email,
-        name: createUserDto.name,
-        password: expect.not.stringContaining(createUserDto.password),
-        createdAt: expect.any(String),
-        updatedAt: expect.any(String),
-      },
+      expect(r.statusCode).toBe(HttpStatus.CREATED);
+      expect(r.body).toEqual({
+        responseTime: expect.any(String),
+        statusCode: HttpStatus.CREATED,
+        message: 'Create a user successfully',
+        data: {
+          id: expect.any(String),
+          email: createUserDto.email,
+          name: createUserDto.name,
+          password: expect.not.stringContaining(createUserDto.password),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+      });
+    });
+
+    it('[POST] /api/v1/users ❌ Failed to create a user because email exists', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'test_email_exist',
+        email: 'test_email_exist@test.com',
+        age: 13,
+        password: 'test123',
+      };
+
+      const { dataSource, userRepository } = getRepoAndDataSource(app);
+
+      await dataSource.transaction((transaction) =>
+        userRepository.create(transaction, createUserDto),
+      );
+
+      const r: request.Response = await request(app.getHttpServer())
+        .post('/api/v1/users')
+        .send(createUserDto);
+
+      expect(r.statusCode).toBe(HttpStatus.BAD_REQUEST);
+      expect(r.body).toEqual({
+        responseTime: expect.any(String),
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: 'Bad Request',
+        message: 'Email already exists',
+      });
+    });
+
+    afterAll(async () => {
+      const q = dataSource.createEntityManager();
+      await q.delete(UserEntityImpl, {});
     });
   });
 
-  it('[POST] /api/v1/users ❌ Failed to create a user because email exists', async () => {
-    const createUserDto: CreateUserDto = {
-      name: 'test_email_exist',
-      email: 'test_email_exist@test.com',
-      age: 13,
-      password: 'test123',
-    };
+  describe('Find user by id', () => {
+    it('[GET] /api/v1/users/:id ✅ Success get a user by id', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'test',
+        email: 'test@test.com',
+        age: 13,
+        password: 'test123',
+      };
 
-    const { dataSource, userRepository } = getRepoAndDataSource(app);
+      const user = await dataSource.transaction<UserEntity>((transaction) =>
+        userRepository.create(transaction, createUserDto),
+      );
 
-    await dataSource.transaction((transaction) =>
-      userRepository.create(transaction, createUserDto),
-    );
+      const r: request.Response = await request(app.getHttpServer()).get(
+        `/api/v1/users/${user.id}`,
+      );
 
-    const r: request.Response = await request(app.getHttpServer())
-      .post('/api/v1/users')
-      .send(createUserDto);
+      expect(r.statusCode).toBe(HttpStatus.OK);
+      expect(r.body).toEqual({
+        responseTime: expect.any(String),
+        statusCode: HttpStatus.OK,
+        message: 'Get a user successfully',
+        data: {
+          id: expect.any(String),
+          email: user.email,
+          name: user.name,
+          password: user.password,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+      });
+    });
 
-    expect(r.statusCode).toBe(HttpStatus.BAD_REQUEST);
-    expect(r.body).toEqual({
-      responseTime: expect.any(String),
-      statusCode: HttpStatus.BAD_REQUEST,
-      error: 'Bad Request',
-      message: 'Email already exists',
+    it('[GET] /api/v1/users/:id ❌ Filed to get a user by id because not found', async () => {
+      const r: request.Response = await request(app.getHttpServer()).get(
+        `/api/v1/users/9d10d81c-39ab-48f1-88e7-3a21725bb245`,
+      );
+
+      expect(r.statusCode).toBe(HttpStatus.NOT_FOUND);
+      expect(r.body).toEqual({
+        responseTime: expect.any(String),
+        statusCode: HttpStatus.NOT_FOUND,
+        error: 'Not Found',
+        message: 'User not found',
+      });
     });
   });
 });
