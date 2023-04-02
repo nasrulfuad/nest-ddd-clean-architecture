3a75844f8c16f88869758f771dff365aee7cd65e
Nasrul Fuad <nasrulfuad08@gmail.com>
2023-04-02 12:53:03 +0700

    refactor(user): :recycle: refactor error message when create user

--- src/modules/user/services/create-user/create-user.service-impl.ts
+++ src/modules/user/services/create-user/create-user.service-impl.ts
@@ -2,7 +2,7 @@ import { UserUseCase } from '@module-user/constants/user-usecase';
 import { UserRepository } from '@module-user/repository/user.repository';
 import { CreateUserDto } from '@module-user/web/dto/create-user.dto';
 import { UserEntity } from '@module-user/web/entities/user.entity';
-import { BadRequestException, Inject } from '@nestjs/common';
+import { ConflictException, Inject } from '@nestjs/common';
 import { EntityManager } from 'typeorm';
 import { CreateUserService } from './create-user.service';
 
@@ -21,7 +21,7 @@ export class CreateUserServiceImpl<T extends EntityManager>
     if (
       await this.userRepository.findByEmail(transaction, createUserDto.email)
     ) {
-      throw new BadRequestException('Email already exists');
+      throw new ConflictException(`Email ${createUserDto.email} is already in use`);
     }
 
     return await this.userRepository.create(transaction, createUserDto);
